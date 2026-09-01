9ea65127d6f59bd5fa1e67c7bb9eb0b8d9883633
zizo <zineb@nadak.ai>
2026-09-01 17:04:51 +0300

    wip: importing potentially necissary packages, storing path to results dir and creating a results file

--- benchmark.py
+++ benchmark.py
@@ -5,6 +5,11 @@ from algorithms.merge_sort import merge_sort
 from algorithms.quick_sort import quick_sort
 from algorithms.python_sorting import python_sort
 
+import csv
+import os
+import random
+import sys
+import time
 
 sizes = [100, 1000, 10000]
 content_types = ["random_0_100", "random_0_10000", "sorted"]
@@ -16,5 +21,8 @@ algorithms = {
     "python_sort": python_sort
 }
 
+resultat_mapp = "results"
+resultat_fil = os.path.join(resultat_mapp, "benchmark_results.csv")
 
+sys.setrecursionlimit(10000)  # prevent recursion limit issues, Python's default recursion limit at n=10000
 
