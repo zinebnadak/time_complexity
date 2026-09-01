7ba9362e4cc13062c9f98a3da42da0707b6e3b9b
zizo <zineb@nadak.ai>
2026-09-01 17:03:10 +0300

    wip: sizes to test, range types and the alorithms from algorithms/ imported into dict

--- benchmark.py
+++ benchmark.py
@@ -1,1 +1,20 @@
 # manual test harness 
+from algorithms.bubble_sort import bubble_sort
+from algorithms.insertion_sort import insertion_sort
+from algorithms.merge_sort import merge_sort
+from algorithms.quick_sort import quick_sort
+from algorithms.python_sorting import python_sort
+
+
+sizes = [100, 1000, 10000]
+content_types = ["random_0_100", "random_0_10000", "sorted"]
+algorithms = {
+    "bubble_sort": bubble_sort,
+    "insertion_sort": insertion_sort,
+    "merge_sort": merge_sort,
+    "quick_sort": quick_sort,
+    "python_sort": python_sort
+}
+
+
+
